784cc30f4bfd9dcc6839a99b153193b853346418
dylansebastianm <dylan.sebastianmte@gmail.com>
2025-02-25 07:51:11 -0300

    url fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,5 @@
-import { Breadcrumb } from "@/components/ui/breadcrumb";
-import { LoanForm } from "@/components/loanForm";
-import { getUserData } from "@/lib/actions";
-import Navbar from "@/components/nav";
+import { redirect } from "next/navigation";
 
-export default async function LoanPage() {
-  const { data: userData, error } = await getUserData();
-
-  return (
-    <>
-      <Navbar />
-      <div className="container mx-auto px-4 py-8">
-        <Breadcrumb
-          items={[
-            { label: "Home", href: "/" },
-            { label: "Préstamos", href: "/prestamos" },
-          ]}
-        />
-        <div className="max-w-3xl mx-auto mt-8">
-          <h1 className="text-3xl font-semibold mb-6" style={{ color: '#262626' }}>
-            Solicitud de Préstamo
-          </h1>
-
-          <h2 className="text-1xl font-semibold mb-6" style={{ color: '#262626' }}>
-            ¡Con Alaska-Préstamos podes acceder a financiamiento exclusivo para comprar tus muebles!
-          </h2>
-          {error ? (
-            <div className="bg-red-50 border border-red-200 rounded-lg p-4 text-red-600">
-              {error}
-            </div>
-          ) : (
-            <LoanForm initialData={userData} />
-          )}
-        </div>
-      </div>
-    </>
-
-  );
+export default function HomePage() {
+  redirect("/prestamos/1"); 
 }
---
+++ app/prestamos/[id]/page.tsx
@@ -0,0 +1,51 @@
+"use client"
+import { Breadcrumb } from "@/components/ui/breadcrumb";
+import { LoanForm } from "@/components/loanForm";
+import { getUserData } from "@/lib/actions";
+import Navbar from "@/components/nav";
+import { useParams } from "next/navigation";
+
+export default async function LoanPage() {
+    const { id } = useParams();
+
+    if (!id) {
+        return (
+            <div>Loading...</div>
+        );
+    }
+
+    const userId = Number(id);
+
+    const { data: userData, error } = await getUserData(userId);
+
+    return (
+        <>
+            <Navbar />
+            <div className="container mx-auto px-4 py-8">
+                <Breadcrumb
+                    items={[
+                        { label: "Home", href: "/" },
+                        { label: "Préstamos", href: "/prestamos" },
+                    ]}
+                />
+                <div className="max-w-3xl mx-auto mt-8">
+                    <h1 className="text-3xl font-semibold mb-6" style={{ color: '#262626' }}>
+                        Solicitud de Préstamo
+                    </h1>
+
+                    <h2 className="text-1xl font-semibold mb-6" style={{ color: '#262626' }}>
+                        ¡Con Alaska-Préstamos podes acceder a financiamiento exclusivo para comprar tus muebles!
+                    </h2>
+                    {error ? (
+                        <div className="bg-red-50 border border-red-200 rounded-lg p-4 text-red-600">
+                            {error}
+                        </div>
+                    ) : (
+                        <LoanForm initialData={userData} />
+                    )}
+                </div>
+            </div>
+        </>
+
+    );
+}
